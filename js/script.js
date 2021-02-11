a6a847b836e90f9b357201049905ebcb996e607f
SebastianZabdyr <zabdyr6@wp.pl>
2021-02-11 01:04:35 +0100

    get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -36,7 +37,15 @@ const titleClickHandler = function(event){
     activeArtilce.classList.remove('active');
   }
 
+  /*console.log('remove active class for Artilces ', activeArticles);*/
+
   /* get 'href' attribute from the clicked link */
+
+  const articleSelector = function(){
+    href = clickedElement.getAtributte('href');
+  }
+
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
