1442ce34a33bda2a23b634d8f1a3c945a2ffcaf9
SebastianZabdyr <>
2021-02-18 23:13:56 +0100

    sript code 1.0

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ const optArticleTagsSelector = '.post-tags .list';
 
 const optArticleAuthorSelector = '.post-author';
 
+const optTagsListSelector = '.tags.list'
+
+
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -208,6 +211,8 @@ function tagClickHandler(event){
 
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
 
   /* find all articles */
 
@@ -276,7 +281,19 @@ function generateTags(){
 
       console.log(html);
 
-      /* check data tags name */
+
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+
+
+
+
 
     }
     /* END LOOP: for each tag */
@@ -289,18 +306,36 @@ function generateTags(){
     tagWrapper.innerHTML = html;
     /*
     const links = document.querySelectorAll('a.active[href^="#tag-"]');
-
     for(let link of links){
-
       link.addEventListener('click', tagClickHandler);
     }
-
     console.log(links);
-
-
 */
     console.log('END LOOP: for every article');
   }
+
+
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+
+
+
 }
 
 generateTags();
@@ -356,13 +391,11 @@ function authorClickHandler(event){
   /* make a new constant "tag" and extract tag from the "href" constant */
   /*
   const tag = href.replace('#tag-', '');
-
   console.log(tag);
 */
   /* find all tag links with class active */
 /*
   const activeAuthors = document.querySelectorAll('.data-author');
-
   console.log(activeAuthors);
 */
   /* START LOOP: for each active tag link */
